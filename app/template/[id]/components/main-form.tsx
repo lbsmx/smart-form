f5cfacf643120a9f37b5eff188141da14819140f
lbsmx <1791631558@qq.com>
2025-04-07 14:56:07 +0800

    dnd-kit

--- app/template/[id]/components/main-form.tsx
+++ app/template/[id]/components/main-form.tsx
@@ -1,71 +1,63 @@
-'use client';
+"use client";
 
-import styles from '@/app/template/[id]/components/styles/main-form.module.css';
-import { Form, Input } from 'antd';
-import DraggableWrapper from './form-item/draggble-wrapper';
-import { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import styles from "@/app/template/[id]/components/styles/main-form.module.css";
+import { Form } from "antd";
+import SortableItem from "@/app/template/[id]/components/sortable-item";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import {
+  SortableContext,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
 export default function MainForm() {
-    const [formList, setFormList] = useState([
-        {
-            id: '1',
-            type: 'textInput',
-        },
-        {
-            id: '2',
-            type: 'textInput',
-        },
-        {
-            id: '3',
-            type: 'textInput',
-        },
-    ]);
+  const [formList, setFormList] = useState([
+    {
+      id: "1",
+      type: "textInput",
+    },
+    {
+      id: "2",
+      type: "textInput",
+    },
+    {
+      id: "3",
+      type: "textInput",
+    },
+  ]);
 
-    const resetFormItem = ({ dragIndex, hoverIndex, item }) => {
-        const copyFormList = [...formList];
-        if (dragIndex === undefined) {
-            const newFormItem = generateFormItem(item);
-            copyFormList.splice(hoverIndex, 0, newFormItem);
-            setFormList(copyFormList);
-        } else {
-            const [removed] = copyFormList.splice(dragIndex, 1);
-            copyFormList.splice(hoverIndex, 0, removed);
-            setFormList(copyFormList);
-        }
+  const generateFormItem = (item) => {
+    return {
+      id: uuidv4(),
+      type: item.type,
     };
+  };
 
-    const generateFormItem = (item) => {
-        return {
-            id: uuidv4(),
-            type: item.type,
-        };
-    };
-
-    return (
-        <div className={styles.formContainer}>
-            <Form
-                className={styles.form}
-                labelCol={{ span: 4 }}
-                wrapperCol={{ span: 16 }}
-                size="large"
-                style={{
-                    padding: '24px',
-                }}
+  return (
+    <div className={styles.formContainer}>
+      <Form
+        className={styles.form}
+        labelCol={{ span: 4 }}
+        wrapperCol={{ span: 16 }}
+        size="large"
+        style={{
+          padding: "24px",
+        }}
+      >
+        <SortableContext
+          items={formList.map((item) => item.id)}
+          strategy={verticalListSortingStrategy}
+        >
+          {formList.map((item, index) => (
+            <SortableItem
+              id={item.id}
+              type={item.type}
+              key={item.id}
+              index={index}
             >
-                {formList.map((item, index) => (
-                    <DraggableWrapper
-                        id={item.id}
-                        type={item.type}
-                        key={item.id}
-                        index={index}
-                        sortFn={resetFormItem}
-                    >
-                        <Form.Item label={item.id}>
-                            <Input></Input>
-                        </Form.Item>
-                    </DraggableWrapper>
-                ))}
-            </Form>
-        </div>
-    );
+            </SortableItem>
+          ))}
+        </SortableContext>
+      </Form>
+    </div>
+  );
 }
--- app/template/[id]/page.tsx
+++ app/template/[id]/page.tsx
@@ -1,24 +1,96 @@
-'use client';
+"use client";
 
 interface TemplateParams {
-    id: string | undefined;
+  id: string | undefined;
 }
 
-import * as React from 'react';
-import styles from '@/app/template/[id]/page.module.css';
-import SideFormItemPanel from './components/side-form-item-panel';
-import MainFormPanel from './components/main-form-panel';
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-export default function Template({ params }: { params: TemplateParams }) {
-    const { id }: { id: string | undefined } = React.use(params);
+import * as React from "react";
+import styles from "@/app/template/[id]/page.module.css";
+import SideFormItemPanel from "./components/side-form-item-panel";
+import MainFormPanel from "./components/main-form-panel";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragOverlay,
+  DropAnimation,
+  defaultDropAnimationSideEffects,
+  UniqueIdentifier,
+  Announcements,
+  ScreenReaderInstructions,
+} from "@dnd-kit/core";
+import { sortableKeyboardCoordinates } from "@dnd-kit/sortable";
+import { createPortal } from "react-dom";
+import { useState } from "react";
+import Item from "./components/Item";
 
-    return (
-        <DndProvider backend={HTML5Backend}>
-            <div className={styles.page}>
-                <SideFormItemPanel></SideFormItemPanel>
-                <MainFormPanel></MainFormPanel>
-            </div>
-        </DndProvider>
-    );
+const screenReaderInstructions: ScreenReaderInstructions = {
+  draggable: `
+      To pick up a sortable item, press the space bar.
+      While sorting, use the arrow keys to move the item.
+      Press space again to drop the item in its new position, or press escape to cancel.
+    `,
+};
+export default function Template({ params }: { params: TemplateParams }) {
+  const { id }: { id: string | undefined } = React.use(params);
+
+  const [activeId, setActiveId] = useState<UniqueIdentifier | null>(null);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+
+  const [portalRoot, setPortalRoot] = useState<HTMLElement | null>(null);
+
+  // 拖拽结束事件
+  const handleDragEnd = (event: any) => {
+    const { active, over } = event;
+  };
+
+  const dropAnimationConfig: DropAnimation = {
+    sideEffects: defaultDropAnimationSideEffects({
+      styles: {
+        active: {
+          opacity: "0.5",
+        },
+      },
+    }),
+  };
+
+  const announcements: Announcements = {
+    onDragStart({ active: { id } }) {},
+    onDragOver({ active, over }) {},
+    onDragEnd({ active, over }) {},
+    onDragCancel({ active: { id } }) {},
+  };
+
+  return (
+    <DndContext
+      accessibility={{
+        announcements,
+        screenReaderInstructions,
+      }}
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <div className={styles.page}>
+        <SideFormItemPanel></SideFormItemPanel>
+        <MainFormPanel></MainFormPanel>
+      </div>
+      {portalRoot &&
+        createPortal(
+          <DragOverlay adjustScale={false} dropAnimation={dropAnimationConfig}>
+            {activeId != null ? <Item id={activeId} /> : null}
+          </DragOverlay>,
+          document.body
+        )}
+    </DndContext>
+  );
 }
